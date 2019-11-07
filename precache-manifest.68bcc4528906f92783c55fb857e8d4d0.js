f94a412653c7cc1440d31f169c8370599f087ff9
Arjun <Arijit Kundu>
2019-11-07 17:50:32 +0530

    Updates

---
+++ precache-manifest.68bcc4528906f92783c55fb857e8d4d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9551a4d97f3a5b9788876e83169f94c5",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "6ba5c9cea419243f3769",
+    "url": "/slider/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d072bbf763b45fe20eb6",
+    "url": "/slider/static/js/2.506ef439.chunk.js"
+  },
+  {
+    "revision": "6ba5c9cea419243f3769",
+    "url": "/slider/static/js/main.b6011f47.chunk.js"
+  },
+  {
+    "revision": "a6b32ae82834d6202e57",
+    "url": "/slider/static/js/runtime~main.5005102e.js"
+  }
+]);
